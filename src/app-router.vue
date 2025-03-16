6237efef316c1555a47a85172ca81302cc487d13
Rodrigo <rodrigo.santos@grupoicts.com.br>
2025-03-15 21:22:37 -0400

    feat: simple routing

---
+++ src/app-router.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from "vue";
+import PortfolioPage from "./pages/PortfolioPage.vue";
+import CreatorPage from "./pages/CreatorPage.vue";
+
+const routes = {
+  "/": PortfolioPage,
+  "/create": CreatorPage,
+};
+
+const currentHash = ref(window.location.hash);
+
+window.addEventListener("hashchange", () => {
+  currentHash.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  return routes[window.location.pathname];
+});
+</script>
+
+<template>
+  <component :is="currentView" />
+</template>
